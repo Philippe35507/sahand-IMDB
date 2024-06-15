785e75a95395770675783488fac0762c8cebdd05
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-06-15 21:12:38 +0000

    Loading
    loading.io

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,8 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page1
-    `
+    `,
+    { next: { revalidate: 60 } }
   );
   const data = await res.json();
 
